17282233e56d26655b45bf5f0aa6cba40e2ce602
Jormdavi <jormdavi@ut.utm.edu>
2016-03-31 21:40:58 -0500

    New main

--- main.cpp
+++ main.cpp
@@ -2,8 +2,26 @@
 
 using namespace std;
 
+int sum(int num){
+
+  int number = num + 1;
+
+  return number;
+
+}
+
 int main(){
 
   cout << "Hello World!" << endl;
+  int num =0;
+
+  cout << "please enter a number" << endl;
+  cin >> num;
+  cout << endl;
+  cout << "Thanks" << endl;
+
+  int num2;
+  num2 = sum(num);
+  cout << "the sum is " << num2 << endl;
 
 }
